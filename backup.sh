8e46ac97250decc192161f66d5a328a395b91d06
Jan Stoetzer <nh13jan@gmail.com>
2013-03-28 15:09:46 +0100

    Basic functionality implemented

--- backup.sh
+++ backup.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
-#----------------------------------------------------------------------------
-# Set your value here
+#-----------------------------------------------------------------------------
+# Set your values here
 
 DIR_DATA=
-SUBDIR_FILES=files/
-SUBDIR_DUMP=dump/
+SUBDIR_FILES=
+SUBDIR_DUMP=
 
 SVN_REPO=
 SVN_USER=
@@ -19,34 +19,63 @@ LOG_DIR=
 LOG_LEVEL=	#0=none, 1=error, 2=warn, 3[default]=info, 4=debug
 
 
-#----------------------------------------------------------------------------
+#-----------------------------------------------------------------------------
 # Do not change anything below this line
 
 add_path_delimiter() {
-
-  local str = $1
-  local last = `echo $str | sed 's#\(.*\)\(.\)$#\2#'`
-  [ $last != '/' ] && echo "$str"/ || echo $str
-
+    local str=$1
+    local last=`echo $str | sed 's#\(.*\)\(.\)$#\2#'`
+    [ $last != '/' ] && echo "$str"/ || echo $str
 }
 
 init_log() {
-
-  [ $LOG_LEVEL ] || LOG_LEVEL=3
-  [ $LOG_LEVEL -lt 0 || $LOG_LEVEL -gt 4 ] && LOG_LEVEL=3
-
-  [ $LOG_DIR ] && LOG_DIR=`add_path_delimiter $LOGDIR` || LOG_DIR=/var/log/redmine_bak_svn/}
-  if [ $LOG_LEVEL -gt 0 ] 
-  then
-    [ ! -e $LOG_DIR ] && mkdir -p $LOG_DIR
-    LOG_FILE="$LOG_PATH"redminebak_`date +%Y%m%d_%H%M%S`
-  fi
-
-
-
-[ `echo /var/log/uschi/ | sed 's#\(.*\)\(.\)$#\2#'` = '/' ] && echo yo || echo nope
-
+    [ $LOG_LEVEL ] || LOG_LEVEL=3
+    [ $LOG_LEVEL -lt 0 || $LOG_LEVEL -gt 4 ] && LOG_LEVEL=3
+    [ $LOG_DIR ] && LOG_DIR=`add_path_delimiter $LOGDIR` || LOG_DIR=/var/log/redmine_bak_svn/
+    if [ $LOG_LEVEL -gt 0 ] 
+    then
+	[ ! -e $LOG_DIR ] && mkdir -p $LOG_DIR
+	LOG_FILE="$LOG_PATH"redminebak_$timestamp
+    fi
 }
 
+#-----------------------------------------------------------------------------
+# Main
+
+#set -x
+
+timestamp=`date +%Y%m%d_%H%M%S`
+
+[ $DIR_DATA ] && DIR_DATA=`add_path_delimiter $DIR_DATA` || DIR_DATA="./" 
+dir_files=$DIR_DATA`add_path_delimiter $SUBDIR_FILES`
+dir_dump=$DIR_DATA`add_path_delimiter $SUBDIR_DUMP`
+
+mysqldump -u$DB_USER -p$DB_PASS $DB_NAME > $dir_dump"dump.sql"
+echo dumping $DB_NAME with $DB_USER $DB_PASS
+
+addcount=0
+for add_file in `svn status $DIR_DATA | grep ^\? | awk '{print $2}'`
+do
+    echo $addcount: adding $add_file 
+    let addcount=$addcount+1    
+    svn add $add_file 2>&1
+done
+
+delcount=0
+for del_file in  `svn status $DIR_DATA | grep ^! | awk '{print $2}'` 
+do 
+    echo $delcount: deleting $del_file 
+    let delcount=$delcount+1    
+    svn delete $del_file 2>&1
+done
+
+commit_msg="Automated Backup $timestamp
+attachment(s) added:   $addcount
+attachment(s) deleted: $delcount
+"
+
+echo commiting
+svn commit --message "$commit_msg" --username $SVN_USER --password $SVN_PASS \
+    --quiet --no-auth-cache --non-interactive --trust-server-cert $DIR_DATA
 
 
